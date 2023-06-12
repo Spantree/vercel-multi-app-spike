50e34ba0e3488d3b1f11be929560d91c9fa7c69b
Jon Dugan <jdugan@spantree.net>
2023-06-12 16:43:07 -0500

    adjust url generation for root application

--- packages/root/next.config.js
+++ packages/root/next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
-    let publicURL = process.env.NEXT_PUBLIC_URL || 'http://127.0.0.1:3001/subapp/:match*';
+    const publicURL = process.env.NEXT_PUBLIC_URL
+      ? `process.env.NEXT_PUBLIC_URL/:match*`
+      : 'http://127.0.0.1:3001/subapp/:match*';
 
     // console.log('Rewrites', process.env);
     return [{ source: '/subapp/:match*', destination: publicURL }];
