8c292327ba7247f0ddc31b870c3c8b92c8f4dd92
Jon Dugan <jdugan@spantree.net>
2023-06-12 16:44:51 -0500

    interpolate the url...

--- packages/root/next.config.js
+++ packages/root/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     const publicURL = process.env.NEXT_PUBLIC_URL
-      ? `process.env.NEXT_PUBLIC_URL/:match*`
+      ? `${process.env.NEXT_PUBLIC_URL}/:match*`
       : 'http://127.0.0.1:3001/subapp/:match*';
 
     // console.log('Rewrites', process.env);
