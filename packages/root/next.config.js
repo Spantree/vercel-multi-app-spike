45f26d18d9cfebe4c639af2db8e0c029aa5d5df8
Jon Dugan <jdugan@spantree.net>
2023-06-13 11:46:45 -0500

    try nextjs with different setting

--- packages/root/next.config.js
+++ packages/root/next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   async rewrites() {
-//     const publicURL = process.env.NEXT_PUBLIC_URL
-//       ? `${process.env.NEXT_PUBLIC_URL}/:match*`
-//       : 'http://127.0.0.1:3001/subapp/:match*';
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    const publicURL = process.env.NEXT_PUBLIC_SUBAPP_URL
+      ? `${process.env.NEXT_PUBLIC_SUBAPP_URL}/:match*`
+      : 'http://127.0.0.1:3001/subapp/:match*';
 
-//     // console.log('Rewrites', process.env);
-//     const rules = [{ source: '/subapp/:match*', destination: publicURL }];
-//     console.log("Rewrites", rules);
-//     return rules;
-//   },
-// };
+    // console.log('Rewrites', process.env);
+    const rules = [{ source: '/subapp/:match*', destination: publicURL }];
+    console.log("Rewrites", rules);
+    return rules;
+  },
+};
 
 // module.exports = nextConfig;
