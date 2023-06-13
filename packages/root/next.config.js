ee27c4d177f1aef4294ff93515a2acbc13f13230
Jon Dugan <jdugan@spantree.net>
2023-06-13 14:19:37 -0500

    remove trailing / for real

--- packages/root/next.config.js
+++ packages/root/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     const publicURL = process.env.NEXT_PUBLIC_SUBAPP_URL
-      ? `${process.env.NEXT_PUBLIC_SUBAPP_URL.trimEnd('/')}/:match*`
+      ? `${process.env.NEXT_PUBLIC_SUBAPP_URL.replace(/\/$/, '')}/:match*`
       : 'http://127.0.0.1:3001/subapp/:match*';
 
     const rules = [{ source: '/subapp/:match*', destination: publicURL }];
