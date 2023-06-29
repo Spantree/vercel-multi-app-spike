f4b594966e986e647d45954415fb49b001747616
Jon Dugan <jdugan@spantree.net>
2023-06-29 14:17:12 -0700

    fix the sub

--- packages/subapp/src/App.tsx
+++ packages/subapp/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import './App.css';
-import logo from './sub.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This is the subapp.</p>
+        <img src={process.env.PUBLIC_URL + '/sub.png'} />
+        <p style={{ color: '#eee' }}>This is the subapp.</p>
         <div
           style={{ border: '1px solid cyan', padding: '1rem', fontSize: '12px', minWidth: '200px' }}
         >
@@ -16,7 +15,13 @@ function App() {
           <pre>GIT AUTHOR: {process.env.REACT_APP_VERCEL_GIT_COMMIT_AUTHOR_LOGIN}</pre>
         </div>
         <p style={{ fontSize: '10px' }}>
-          <a href="https://www.freepik.com/free-vector/hand-drawn-retro-submarine_2783036.htm#query=submarine%20line%20art&position=0&from_view=search&track=ais">
+          <a
+            style={{
+              color: '#fff',
+              textDecoration: 'none',
+            }}
+            href="https://www.freepik.com/free-vector/hand-drawn-retro-submarine_2783036.htm#query=submarine%20line%20art&position=0&from_view=search&track=ais"
+          >
             Image by rawpixel.com
           </a>{' '}
           on Freepik
